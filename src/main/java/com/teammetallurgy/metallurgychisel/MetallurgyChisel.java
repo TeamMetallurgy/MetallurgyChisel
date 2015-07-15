0f2d9db964c918a9855c3ccb182d4d4089476a10
Glassmaker <JeanGlassmaker@mail.com>
2015-07-15 05:12:31 +0300

    Adds Deps

--- src/main/java/com/teammetallurgy/metallurgychisel/MetallurgyChisel.java
+++ src/main/java/com/teammetallurgy/metallurgychisel/MetallurgyChisel.java
@@ -9,11 +9,12 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = MetallurgyChisel.MODID, version = MetallurgyChisel.VERSION)
+@Mod(modid = MetallurgyChisel.MODID, version = MetallurgyChisel.VERSION, dependencies = MetallurgyChisel.DEPS)
 public class MetallurgyChisel
 {
     public static final String MODID = "metallurgychisel";
     public static final String VERSION = "0.1";
+    public static final String DEPS = "after:Metallurgy;after:chisel";
 
     @Instance(MODID)
     public static MetallurgyChisel instance;
