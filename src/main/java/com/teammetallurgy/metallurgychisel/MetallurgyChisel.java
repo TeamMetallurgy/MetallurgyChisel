af78c147059b342e19ab39f3ea7a81b5d86a0c97
Glassmaker <JeanGlassmaker@mail.com>
2015-09-21 22:53:25 +0300

    Adds in-class version number replacing

--- src/main/java/com/teammetallurgy/metallurgychisel/MetallurgyChisel.java
+++ src/main/java/com/teammetallurgy/metallurgychisel/MetallurgyChisel.java
@@ -17,7 +17,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 public class MetallurgyChisel
 {
     public static final String MODID = "metallurgychisel";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "%version%";
     public static final String DEPS = "required-after:Metallurgy;required-after:chisel";
 
     @Instance(MODID)
